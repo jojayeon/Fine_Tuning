eefee2fe4eb922b9daae0f3e0b495c5e4290ae07
jojayeon <jojayeon6152@gmail.com>
2024-10-21 16:39:54 +0900

    Fix | llama3.2,py
    라마불러와서 답변받기 성공
    현재 진행 중에 토큰을 padding하는 것을 추가

--- berttest/llama3.2.py
+++ berttest/llama3.2.py
@@ -1,4 +1,4 @@
-from transformers import PreTrainedTokenizerFast , 
+from transformers import PreTrainedTokenizerFast , LlamaForCausalLM
 import torch
 
 # 모델과 토크나이저 로드
@@ -12,12 +12,15 @@ model.eval()
 # 테스트 입력 텍스트
 input_text = "인공지능에 대해 어떻게 생각하시나요?"
 
+# pad_token을 eos_token으로 설정 # 아래 input에서 패팅을 사용하려면 패팅을 지정해줘야하기떄무네 설정
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
 # 입력 텍스트를 토큰화
-input_ids = tokenizer(input_text, return_tensors='pt').input_ids
+inputs = tokenizer(input_text, return_tensors='pt', padding=True, truncation=True)
 
 # 모델 예측
 with torch.no_grad():
-    outputs = model.generate(input_ids, max_length=50)  # 최대 출력 길이 설정
+    outputs = model.generate( inputs['input_ids'], attention_mask=inputs['attention_mask'], max_length=100)  # 최대 출력 길이 설정
 
 # 출력 토큰을 텍스트로 변환
 output_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
